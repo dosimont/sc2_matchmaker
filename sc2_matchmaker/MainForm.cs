3ec4fd574b7b02de540e03057779f93de45bcbed
Damien Dosimont <damien.dosimont@imag.fr>
2015-09-03 16:41:55 +0200

    copyright message

--- sc2_matchmaker/MainForm.cs
+++ sc2_matchmaker/MainForm.cs
@@ -2,18 +2,17 @@
 
 SC 2 Matchmaker is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+the Free Software Foundation, version 3 of the License.
 
-Foobar is distributed in the hope that it will be useful,
+SC 2 Matchmaker is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see<http://www.gnu.org/licenses/>.*/
+along with Foobar.  If not, see<http://www.gnu.org/licenses/>.
 
-
+Copyright Damien Dosimont, 2015 */
 
 using System;
 using System.Collections.Generic;
--- sc2_matchmaker/Player.cs
+++ sc2_matchmaker/Player.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*This file is part of SC 2 Matchmaker.
+
+SC 2 Matchmaker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+SC 2 Matchmaker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see<http://www.gnu.org/licenses/>.
+
+Copyright Damien Dosimont, 2015 */
+
+using System;
 
 namespace sc2_matchmaker
 {
